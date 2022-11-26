bcc8bb26d61485b0ff998e9e2f2623d42f5819c2
MSunaam <95274859+MSunaam@users.noreply.github.com>
2022-11-26 12:15:13 +0500

    Refactored the file structure
    Refactored file structure; moved convertToBinary function to supporting functions for access in other files as well

--- Content_Array.py
+++ Content_Array.py
@@ -1,15 +1,4 @@
-def convertToBinary(value, size=16):
-    # Default argument 16 for whole register
-    if size == 16 or size == 8:
-        binary = bin(value)
-        str_bin = str(binary)
-        str_bin = str_bin[2:]
-        if len(str_bin) > size:
-            print(f"Size Mismatch: Register can hold {size}-bits only")
-        else:
-            str_bin = "0" * (size - len(str_bin)) + str_bin
-            # filling the string with 0s
-            return [int(x) for x in str_bin]
+from Supporitng_Functions import convertToBinary
 
 
 class Content_Array:
