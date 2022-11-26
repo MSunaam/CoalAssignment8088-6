ef1660779669eb96c5b5fd0e1c8a832b6cfc0cea
MSunaam <95274859+MSunaam@users.noreply.github.com>
2022-11-26 13:25:10 +0500

    Overhauled Register Class
    I think I went a little overboard, going to simplify a litte

--- Register.py
+++ Register.py
@@ -6,52 +6,86 @@ class Register:
     def __init__(self, name, enableLowHigh):
         self.registerLowHigh = enableLowHigh
         # For future reference to check if register allows accessing low and high or not
-        self.low = None
-        # If register allows accessing low and high; store low in low
-        self.high = None
-        # If register allows accessing low and high; store high in high
         self.name = name
         if enableLowHigh:
             self.content = Content_Array()
             # Allows access to individual 8-bits
+            self.higherSubReg = subRegister(name[0] + 'H', self.content.getHigh())
+            self.lowerSubReg = subRegister(name[0] + 'L', self.content.getLow())
         else:
             self.content = Content_Array_16()
             # Does not allow access to individual 8-bits
 
     def getLow(self):
-        # Get Low Contents
+        # Get the content of the lower Part
         if self.registerLowHigh:
-            self.low = self.content.getLow()
-            return self.low
+            # 8-bit accessible register
+            return self.lowerSubReg.getData()
         else:
-            print("Register Error: Register does not allow accessing Low and High")
+            print("This Register does not support access of Lower 8-bits")
 
     def getHigh(self):
-        # Get High Contents
+        # Get the content of the higher Part
         if self.registerLowHigh:
-            self.high = self.content.getHigh()
-            return self.high
+            # 8-bit accessible register
+            return self.higherSubReg.getData()
         else:
-            print("Register Error: Register does not allow accessing Low and High")
+            print("This Register does not support access of higher 8-bits")
+
+    def setLow(self, values):
+        # Set the content of the lower Part
+        if self.registerLowHigh:
+            # 8-bit accessible register
+            # update the content array as well
+            self.content.inputL(values)
+            # This approach used because content array will automatically convert to binary and store in lower 8-bits
+            # Also validation will be done
+            self.lowerSubReg.setData(self.content.getLow())
+        else:
+            print("This Register does not support access of lower 8-bits")
+
+    def setHigh(self, values):
+        # Set the content of the higher Part
+        if self.registerLowHigh:
+            # 8-bit accessible register
+            # update the content array as well
+            self.content.inputH(values)
+            # This approach used because content array will automatically convert to binary and store in higher 8-bits
+            # Also validation will be done
+            self.higherSubReg.setData(self.content.getHigh())
+        else:
+            print("This Register does not support access of higher 8-bits")
+
+    def setData(self, values):
+        # Set the data of the whole register
+        self.content.input(values)
+        # Now change the values of subRegisters if exist
+        if self.registerLowHigh:
+            # SubRegisters Exist
+            self.higherSubReg.setData(self.content.getHigh())
+            self.lowerSubReg.setData(self.content.getLow())
 
     def getData(self):
-        # Get Register Contents
         return self.content.getData()
 
-    def input(self, value):
-        # Update Register Contents
-        self.content.input(value)
+    def printContent(self):
+        self.content.print()
 
-    def inputLow(self, value):
-        # Update Low Contents
-        if self.registerLowHigh:
-            self.content.inputL(value)
-        else:
-            print("Register Error: Register does not allow accessing Low and High")
 
-    def inputHigh(self, value):
-        # Update High Contents
-        if self.registerLowHigh:
-            self.content.inputH(value)
-        else:
-            print("Register Error: Register does not allow accessing Low and High")
+class subRegister:
+
+    # This class is for storing lower and higher parts of Registers
+    def __init__(self, name, values=None):
+        # Initialize the subRegister
+        if values is None:
+            values = []
+        self.array = values
+        self.name = name
+
+    def getData(self):
+        # Get the data from sub-Register
+        return self.array
+
+    def setData(self, values):
+        # Set the values
+        self.array = values
--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 from Register import Register
+
+AX = Register("AX", True)
+AX.setData(4000)
+AX.printContent()
+print(AX.getHigh())
+print(AX.getLow())
