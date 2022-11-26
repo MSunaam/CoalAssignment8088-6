50789a324143c1b07cc0617b398614ac11bf1a98
MSunaam <95274859+MSunaam@users.noreply.github.com>
2022-11-26 12:10:03 +0500

    Reinforcing 8-bit accessibility in Register
    Reinforced accessibility of 8-bits low and high for accessible register and removed access of such bits in non-accessible register

--- Register.py
+++ Register.py
@@ -4,7 +4,12 @@ from Content_Array import Content_Array_16
 
 class Register:
     def __init__(self, name, enableLowHigh):
+        self.registerLowHigh = enableLowHigh
+        # For future reference to check if register allows accessing low and high or not
         self.low = None
+        # If register allows accessing low and high; store low in low
+        self.high = None
+        # If register allows accessing low and high; store high in high
         self.name = name
         if enableLowHigh:
             self.content = Content_Array()
@@ -14,4 +19,15 @@ class Register:
             # Does not allow access to individual 8-bits
 
     def getLow(self):
-        self.low = self.content.getLow()
+        if self.registerLowHigh:
+            self.low = self.content.getLow()
+            return self.low
+        else:
+            print("Register Error: Register does not allow accessing Low and High")
+
+    def getHigh(self):
+        if self.registerLowHigh:
+            self.high = self.content.getHigh()
+            return self.high
+        else:
+            print("Register Error: Register does not allow accessing Low and High")
--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-from Register import
+from Register import Register
 
+AX = Register("AX", True)
+print(AX.getLow())
+print(AX.getHigh())
