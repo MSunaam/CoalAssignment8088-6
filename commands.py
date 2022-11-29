fd0a14750e124cdedf14bd846c19bcaeaf198fb7
MSunaam <95274859+MSunaam@users.noreply.github.com>
2022-11-29 16:00:06 +0500

    ror and rol

--- commands.py
+++ commands.py
@@ -1,4 +1,5 @@
 from Register_Instances import *
+from collections import deque
 from Supporitng_Functions import checkRegister, returnDecimal
 
 
@@ -44,3 +45,35 @@ def dec(register):
         value -= 2 ** registerSize
 
     register.input(value)
+
+
+def rol(register, times):
+    if times > 0:
+        rotate(register, -times)
+    else:
+        rotate(register, -times)
+
+
+def ror(register, times):
+    if times > 0:
+        rotate(register, times)
+    else:
+        rotate(register, times)
+
+
+def rotate(register, times):
+    # If times is positive it will rotate right and if it is negative it will rotate left
+    # Register values will be rotated right times times
+    # Get Register value
+    check, register = checkRegister(register)
+    if not check:
+        print("Invalid Register")
+        return
+    # Get register data
+    values = register.getData()
+    # If times greater than register size subtract checked on Masm
+    if times > register.size:
+        times = times - register.size
+    deq = deque(register.content.content)
+    deq.rotate(times)
+    register.content.content = list(deq)
--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 from commands import *
 from Register_Instances import *
+
+AX.input(5)
+AX.printContent()
+rol('ax', -17)
+AX.printContent()
