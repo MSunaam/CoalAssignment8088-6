d1bc7e4c09cbe25cea477716b69001d0af05cdfa
Rohail822 <rohail5376@gmail.com>
2022-12-05 00:30:00 +0500

    update supporting functions
    added shift left opcode 4

--- Supporitng_Functions.py
+++ Supporitng_Functions.py
@@ -119,3 +119,55 @@ def shr(machineCode, rm, times, isMemory):
                 deq.appendleft(0)
             memory.array = list(deq)
 
+
+
+def shl(machineCode, rm, times, isMemory):
+    # Set Opcode
+    machineCode.setOpcode('0100')
+    # Set imm
+    imm = bin(times if times > 0 else times + (1 << 8))[2:]
+    imm = imm.zfill(8)
+    machineCode.setImm(imm)
+    # Shift the Register/Memory left times times
+    # If times is negative use SHR
+    if times < 0:
+        shr(machineCode, rm, abs(times), isMemory)
+    if not isMemory:
+        # rm is register
+        check, register = checkRegister(rm, machineCode)
+        if not check:
+            print('Register Incorrect')
+            return
+        # Register Correct
+        # Set Register code
+        machineCode.setReg(register.code)
+        # Get Register Values
+        values = register.getData()
+        # Using Deque because pop has O(1) complexity
+
+        deq = deque(values)
+        for x in range(times):
+            if x == times - 1:
+                # Last Iteration Store bit in CF
+                flags.CF(deq[0])
+            deq.popleft()
+            deq.append(0)
+        register.inputList(list(deq))
+    else:
+        # rm is memory
+        check, memory = checkMemory(rm, machineCode)
+        if not check:
+            # rm is incorrect memory
+            print("Memory out of range")
+            return
+        else:
+            # Correct memory
+            # Using deque because of less complexity
+            deq = deque(memory.array)
+            for x in range(times):
+                if x == times - 1:
+                    # Last Iteration store the bit in CF
+                    flags.CF(deq[0])
+                deq.popleft()
+                deq.append(0)
+            memory.array = list(deq)
