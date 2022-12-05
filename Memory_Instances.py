479dc80dcbd474ee99be2795a9513d5dc0a69ab5
madeelshaloli <muadeel.bscs21seecs@seecs.edu.pk>
2022-12-05 09:38:01 +0500

    Memory array

--- Memory_Instances.py
+++ Memory_Instances.py
@@ -23,3 +23,24 @@ ES = Register('es', False, 7)  # 111
 Reg = {'ax': AX, 'bx': BX, 'cx': CX, 'dx': DX, 'ds': DS, 'cs': CS, 'ss': SS, 'es': ES}
 
 SubReg = {'ah': AH, 'al': AL, 'bh': BH, 'bl': BL, 'ch': CH, 'cl': CL, 'dh': DH, 'dl': DL}
+
+
+from memory import Memory_Array
+
+mem = {'0': Memory_Array(0),
+       '1': Memory_Array(1),
+       '2': Memory_Array(2),
+       '3': Memory_Array(3),
+       '4': Memory_Array(4),
+       '5': Memory_Array(5),
+       '6': Memory_Array(6),
+       '7': Memory_Array(7),
+       '8': Memory_Array(8),
+       '9': Memory_Array(9),
+       'A': Memory_Array(10),
+       'B': Memory_Array(11),
+       'C': Memory_Array(12),
+       'D': Memory_Array(13),
+       'E': Memory_Array(14),
+       'F': Memory_Array(15),
+       }
